853274e3d4a507f6ae7289acaa34cc0a47718b6c
Hào Trương <emlahao123@outlook.com>
2023-04-18 19:57:21 +0700

    Add project files.

---
+++ Models/Employees.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EmployeeManager.Models
+{
+    [Table("Employees")]
+    public class Employees
+    {
+        [Required(ErrorMessage = "Employee ID is required")]
+        public int EmployeeID { get; set; }
+        [Required(ErrorMessage = "Eployee last name is required")]
+        public string LastName { get; set; }
+        [Required(ErrorMessage = "Eployee first name is required")]
+        public string FirstName { get; set; }
+        [Required(ErrorMessage = "Eployee title is required")]
+        public string Title { get; set; }
+        [Required(ErrorMessage = "Eployee birth date is required")]
+        public DateTime BirthDate { get; set; }
+        [Required(ErrorMessage = "Eployee hire date is required")]
+        public DateTime HireDate { get; set; }
+        [Required(ErrorMessage = "Eployee country is required")]
+        public string Country { get; set; }
+        [Required(ErrorMessage = "Eployee notes is required")]
+        public string Notes { get; set; }
+    }
+}
